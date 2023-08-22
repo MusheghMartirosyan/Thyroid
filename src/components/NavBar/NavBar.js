46eacd17e4f2423a6b38aa2abea6153173929b81
Musegh <118133145+MusheghMartirosyan@users.noreply.github.com>
2023-08-22 21:11:27 +0400

    added responsive UI

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,27 +6,28 @@ import "./NavBar.css"
 
 const NavBar = () => {
   return (
-    <>
-      <Navbar className='navbar'>
-        <Container>
-            <Nav className="me-auto nav-items">
-                <Nav.Link href="#home">Thyroid gland</Nav.Link>
-                <Nav.Link href="#features">IR treatment</Nav.Link>
-                <Nav.Link href="#pricing">Ablation</Nav.Link>
-                <Nav.Link href="#pricing">Biopsy</Nav.Link>
-                <Nav.Link href="#pricing">Useful articles</Nav.Link>
-                <Nav.Link href="#pricing">Doctors</Nav.Link>
-                <Nav.Link href="#pricing">Gallery</Nav.Link>
-                <Nav.Link href="#pricing">Where to find us</Nav.Link>
-                <button className='nav-button btn'>Submit my medical records</button>
-                <NavDropdown title="ENG" id="nav-dropdown">
-                    <NavDropdown.Item eventKey="4.1">հայերեն</NavDropdown.Item>
-                    <NavDropdown.Item eventKey="4.2">русский</NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar collapseOnSelect className='navbar' expand='sm'>
+      <Container>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto nav-items'>
+            <Nav.Link href='#home'>Thyroid gland</Nav.Link>
+            <Nav.Link href='#features'>IR treatment</Nav.Link>
+            <Nav.Link href='#pricing'>Ablation</Nav.Link>
+            <Nav.Link href='#pricing'>Biopsy</Nav.Link>
+            <Nav.Link href='#pricing'>Useful articles</Nav.Link>
+            <Nav.Link href='#pricing'>Doctors</Nav.Link>
+            <Nav.Link href='#pricing'>Gallery</Nav.Link>
+            <Nav.Link href='#pricing'>Where to find us</Nav.Link>
+            <button className='nav-button btn'>Submit my medical records</button>
+            <NavDropdown title='ENG' id='nav-dropdown'>
+              <NavDropdown.Item eventKey='4.1'>հայերեն</NavDropdown.Item>
+              <NavDropdown.Item eventKey='4.2'>русский</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
